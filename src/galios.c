1c3b77a0e786aacccc13946d1293526c8563740c
Raaaainbow <seba7204@gmail.com>
2025-06-21 12:42:53 +0200

    log table function fixed array out of bounds issues

--- src/galios.c
+++ src/galios.c
@@ -1,30 +1,26 @@
 #include <stdint.h>
 #define gf_bits 8
-#define prim_poly 497
-
+#define prim_poly 0b100011101  // z^8 + z^4 + z^3 + z^2 + 1 F(2^8)
 uint8_t log_table[256];
 uint8_t antilog_table[256];
+
+uint8_t antilog_table[255];
 
 void init_gf_tables() {
     uint8_t a = 1;
 
-    for (uint8_t i = 0; i < 255 ; ++i) {
+    for (uint8_t i = 0; i < 255; ++i) {
         antilog_table[i] = a;
-
-        uint8_t hi_bit = a & 128;
+        log_table[a] = i;
+        
+        uint8_t hi_bit = a & 0x80;
         a <<= 1;
-        if (hi_bit == 128) {
-            a ^= 283;
+        if (hi_bit) {
+            a ^= 0x1D;
         }
-
-        a ^= antilog_table[i];
-        log_table[antilog_table[i]] = i;
     }
-
-    antilog_table[255] = antilog_table[0];
-
+    
     log_table[0] = 0;
-    log_table[255] = 1;
 }
 
 uint8_t gf_add(uint8_t a, uint8_t b) {
@@ -40,7 +36,7 @@ uint8_t gf_mult(uint8_t a,uint8_t b) {
     return antilog_table[log_result];
 }
 
-uint8_t df_div(uint8_t a,uint8_t b) {
+uint8_t gf_div(uint8_t a,uint8_t b) {
     if (a == 0) return 0;
     if (b == 0) return 0;
     
