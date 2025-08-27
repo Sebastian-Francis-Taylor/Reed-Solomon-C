cebb618621143373380114ff3cb903b57d12e029
Sebastian-Francis-Taylor <me@sebastian-taylor.com>
2025-08-27 14:07:09 +0200

    encoder working, not memory safe

--- src/rs_encoder.c
+++ src/rs_encoder.c
@@ -3,64 +3,96 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define MAX_ERRORS 16
-#define PARITY_SYMBOLS 2 * MAX_ERRORS
+#define PARITY_SYMBOLS_AMOUNT 2 * MAX_ERRORS
 #define PRIMITIVE_ELEMENT 2
-#define GENERATOR_POLYNOMIAL {45, 216, 239, 24, 253, 104, 27, 40, 107, 50, 163, 210, 227, 134, 224, 158, 119, 13, 158, 1, 238, 164, 82, 43, 15, 232, 246, 142, 50, 189, 29, 232, 1}
-#define GENERATOR_LEN 33
-#define FIELD_SIZE 255
 
-uint8_t *shift_polynomial_left(uint8_t *poly, int poly_len, int shift_number) {
-    uint8_t *shifted_polynomial = calloc(poly_len + shift_number, sizeof(uint8_t));
-    for (int i = 0; i < poly_len; ++i) {
-        shifted_polynomial[i] = poly[i];
+// long so that it does not need to be padded during runtime
+static const uint8_t generator_poly[255] = {
+    1,  232, 29,  189, 50, 142, 246, 232, 15, 43, 82, 164, 238, 1, 158, 13, 119, 158, 224, 134, 227, 210, 163, 50, 107,
+    40, 27,  104, 253, 24, 239, 216, 45,  0,  0,  0,  0,   0,   0, 0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  0,
+    0,  0,   0,   0,   0,  0,   0,   0,   0,  0,  0,  0,   0,   0, 0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  0,
+    0,  0,   0,   0,   0,  0,   0,   0,   0,  0,  0,  0,   0,   0, 0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  0,
+    0,  0,   0,   0,   0,  0,   0,   0,   0,  0,  0,  0,   0,   0, 0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  0,
+    0,  0,   0,   0,   0,  0,   0,   0,   0,  0,  0,  0,   0,   0, 0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  0,
+    0,  0,   0,   0,   0,  0,   0,   0,   0,  0,  0,  0,   0,   0, 0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  0,
+    0,  0,   0,   0,   0,  0,   0,   0,   0,  0,  0,  0,   0,   0, 0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  0,
+    0,  0,   0,   0,   0,  0,   0,   0,   0,  0,  0,  0,   0,   0, 0,   0,  0,   0,   0,   0,   0,   0,   0,   0,  0,
+    0,  0,   0,   0,   0,  0,   0,   0,   0,  0,  0,  0,   0,   0, 0,   0,  0};
+
+static const int field_size = 255;
+
+uint8_t *extend_poly(const uint8_t *poly, int len, int extra) {
+    uint8_t *out = calloc(len + extra, 1);
+    for (int i = 0; i < len; ++i) {
+        out[i] = poly[i];
     }
+    return out;
+}
 
-    return shifted_polynomial;
+uint8_t *shift_poly(const uint8_t *poly, int len, int k) {
+    uint8_t *out = calloc(len + k, 1);
+    for (int i = 0; i < len; ++i) {
+        out[i + k] = poly[i];
+    }
+    return out;
 }
 
 uint8_t *rs_encode(uint8_t *info_poly, int info_poly_len) {
 
-    uint8_t *shifted_info_poly = shift_polynomial_left(info_poly, info_poly_len, PARITY_SYMBOLS);
-    int shifted_info_poly_len = info_poly_len + PARITY_SYMBOLS;
+    // printf("info_poly: ");
+    // for (int i = 0; i < info_poly_len; ++i) {
+    // printf("%d ", info_poly[i]);
+    // }
+    // printf("\n");
+    // printf("\n");
 
-    printf("shifted_info_poly: ");
-    for (int i = 0; i < shifted_info_poly_len; ++i) {
-        printf("%d ", shifted_info_poly[i]);
+    uint8_t *padded_info_poly = shift_poly(info_poly, info_poly_len, PARITY_SYMBOLS_AMOUNT);
+
+    // printf("padded_info_poly_len: %d\n", info_poly_len + PARITY_SYMBOLS_AMOUNT);
+    // printf("padded_info_poly: ");
+    // for (int i = 0; i < (info_poly_len + PARITY_SYMBOLS_AMOUNT); ++i) {
+    // printf("%d ", padded_info_poly[i]);
+    // }
+    // printf("\n");
+    // printf("\n");
+
+    printf("padded_info_poly (dividend): ");
+    for (int i = 0; i < field_size; ++i) {
+        printf("%d ", padded_info_poly[i]);
     }
     printf("\n");
+    printf("\n");
 
-    uint8_t generator[] = GENERATOR_POLYNOMIAL;
-    int generator_len = GENERATOR_LEN;
-    uint8_t *shifted_generator = shift_polynomial_left(generator, generator_len, FIELD_SIZE - generator_len);
-    int generator_deg = poly_degree(shifted_generator, FIELD_SIZE - generator_len);
-    printf("generator_deg: %d\n", generator_deg);
+    printf("extended_generator (divisor): ");
+    for (int i = 0; i < field_size; ++i) {
+        printf("%d ", generator_poly[i]);
+    }
+    printf("\n");
+    printf("\n");
 
-    poly_div_result division_result = poly_div(shifted_info_poly, shifted_generator, FIELD_SIZE);
+    poly_div_result division_result = poly_div(padded_info_poly, (uint8_t *)generator_poly, field_size);
     uint8_t *parity_symbols = division_result.remainder;
-    int parity_symbols_len = 32;
 
     printf("parity_symbols: ");
-    for (int i = 0; i < parity_symbols_len; ++i) {
+    for (int i = 0; i < 32; ++i) {
         printf("%d ", parity_symbols[i]);
     }
     printf("\n");
-
-    uint8_t *encoded_message = calloc(FIELD_SIZE, sizeof(uint8_t));
-
-    printf("info_poly_len: %d\n", info_poly_len);
-    for (int i = 0; i < info_poly_len; ++i) {
-        encoded_message[i] = info_poly[i];
-    }
-
-    int iter = PARITY_SYMBOLS - 1;
-    for (int i = info_poly_len; i < FIELD_SIZE; ++i) {
-        encoded_message[i] = parity_symbols[iter];
-        iter--;
-    }
     printf("\n");
+
+    uint8_t *encoded_message = calloc(field_size, sizeof(uint8_t));
+
+    for (int i = 0; i < PARITY_SYMBOLS_AMOUNT; ++i) {
+        encoded_message[i] = parity_symbols[i];
+    }
+
+    int iter = 0;
+    for (int i = PARITY_SYMBOLS_AMOUNT; i < field_size; ++i) {
+        encoded_message[i] = info_poly[iter];
+        ++iter;
+    }
     // memcpy(encoded_message, shifted_info_poly, info_poly_len);
     // memcpy(encoded_message + info_poly_len, division_result.remainder, PARITY_SYMBOLS);
 
--- src/rs_encoder.h
+++ src/rs_encoder.h
@@ -13,7 +13,9 @@
 #define GENERATOR_LEN 33
 #define FIELD_SIZE 255
 
-uint8_t *shift_polynomial_left(uint8_t *poly, int poly_len, int shift_number);
+void reverse_array(uint8_t *arr, int len);
+uint8_t *extend_poly(const uint8_t *poly, int len, int extra);
+uint8_t *shift_poly(const uint8_t *poly, int len, int k);
 uint8_t *rs_encode(uint8_t *info_poly, int info_poly_len);
 
 #endif
